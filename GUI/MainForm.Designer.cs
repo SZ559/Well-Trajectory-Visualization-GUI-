4da86efb252e41325c7d04739eae3aa1020644a9
cici zhang <sz559@cornell.edu>
2020-05-17 21:12:14 +0800

    Update data model. Consider the defualt situation

--- GUI/MainForm.Designer.cs
+++ GUI/MainForm.Designer.cs
@@ -37,6 +37,8 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.closeTheCurrentTabPageToolStripButton = new System.Windows.Forms.ToolStripButton();
@@ -48,13 +50,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.tabControl = new System.Windows.Forms.TabControl();
-            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.imageSourceReference = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.referenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -76,10 +76,8 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(2, 1, 0, 1);
-            this.menuStrip1.Size = new System.Drawing.Size(1070, 24);
-
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 60);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -90,7 +88,7 @@
             this.saveAsToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(87, 45);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(87, 58);
             this.fileToolStripMenuItem.Text = "&File";
             // 
             // openFileToolStripMenuItem
@@ -120,8 +118,24 @@
             // viewToolStripMenuItem
             // 
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(106, 45);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(106, 58);
             this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewSourceToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(104, 58);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // viewSourceToolStripMenuItem
+            // 
+            this.viewSourceToolStripMenuItem.Name = "viewSourceToolStripMenuItem";
+            this.viewSourceToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F7)));
+            this.viewSourceToolStripMenuItem.Size = new System.Drawing.Size(463, 54);
+            this.viewSourceToolStripMenuItem.Text = "View &Source";
+            this.viewSourceToolStripMenuItem.Click += new System.EventHandler(this.viewSourceToolStripMenuItem_Click);
             // 
             // toolStrip1
             // 
@@ -129,10 +143,9 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.closeTheCurrentTabPageToolStripButton});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 60);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(1070, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1070, 62);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -143,7 +156,7 @@
             this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(58, 20);
+            this.toolStripButton1.Size = new System.Drawing.Size(58, 55);
             this.toolStripButton1.Text = "Open";
             this.toolStripButton1.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
             // 
@@ -154,8 +167,8 @@
             this.closeTheCurrentTabPageToolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.closeTheCurrentTabPageToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.closeTheCurrentTabPageToolStripButton.Name = "closeTheCurrentTabPageToolStripButton";
-            this.closeTheCurrentTabPageToolStripButton.Size = new System.Drawing.Size(23, 22);
-            this.closeTheCurrentTabPageToolStripButton.Text = "Close Tab Page";
+            this.closeTheCurrentTabPageToolStripButton.Size = new System.Drawing.Size(58, 55);
+            this.closeTheCurrentTabPageToolStripButton.Text = "Close Current Tab Page";
             this.closeTheCurrentTabPageToolStripButton.Click += new System.EventHandler(this.CloseTheCurrentTabPageToolStripButton_Click);
             // 
             // statusStrip1
@@ -171,7 +184,7 @@
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 122);
             this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.splitContainer1.Name = "splitContainer1";
             // 
@@ -183,7 +196,7 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.defaultPagePanel);
             this.splitContainer1.Panel2.Controls.Add(this.tabControl);
-            this.splitContainer1.Size = new System.Drawing.Size(1070, 518);
+            this.splitContainer1.Size = new System.Drawing.Size(1070, 445);
             this.splitContainer1.SplitterDistance = 151;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 3;
@@ -199,7 +212,7 @@
             treeNode1.Text = "Wells";
             this.wellsTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode1});
-            this.wellsTreeView.Size = new System.Drawing.Size(151, 518);
+            this.wellsTreeView.Size = new System.Drawing.Size(151, 445);
             this.wellsTreeView.TabIndex = 0;
             this.wellsTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.WellsTreeView_NodeMouseClick);
             this.wellsTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.WellsTreeView_NodeMouseDoubleClick);
@@ -223,7 +236,7 @@
             this.defaultPagePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.22494F));
             this.defaultPagePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.22494F));
             this.defaultPagePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.22494F));
-            this.defaultPagePanel.Size = new System.Drawing.Size(914, 518);
+            this.defaultPagePanel.Size = new System.Drawing.Size(914, 445);
             this.defaultPagePanel.TabIndex = 0;
             // 
             // label4
@@ -231,9 +244,9 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(3, 475);
+            this.label4.Location = new System.Drawing.Point(3, 391);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(908, 21);
+            this.label4.Size = new System.Drawing.Size(908, 52);
             this.label4.TabIndex = 3;
             this.label4.Text = "View Source Files: Ctrl+F7";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -243,9 +256,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(3, 285);
+            this.label1.Location = new System.Drawing.Point(3, 228);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(908, 21);
+            this.label1.Size = new System.Drawing.Size(908, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "Load Trajectory: Ctrl+Shift+O";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -255,9 +268,9 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(3, 348);
+            this.label2.Location = new System.Drawing.Point(3, 282);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(908, 21);
+            this.label2.Size = new System.Drawing.Size(908, 52);
             this.label2.TabIndex = 1;
             this.label2.Text = "Save View: Ctril+Shift+S";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -267,45 +280,12 @@
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(3, 411);
+            this.label3.Location = new System.Drawing.Point(3, 336);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(908, 21);
+            this.label3.Size = new System.Drawing.Size(908, 52);
             this.label3.TabIndex = 2;
             this.label3.Text = "Open Customized View Editor: Ctrl+Shift+E";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // tabControl
-            // 
-            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl.Location = new System.Drawing.Point(0, 0);
-            this.tabControl.Margin = new System.Windows.Forms.Padding(9, 11, 9, 11);
-            this.tabControl.Multiline = true;
-            this.tabControl.Name = "tabControl";
-            this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(914, 518);
-            this.tabControl.TabIndex = 0;
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.FileName = "openFileDialog";
-            this.openFileDialog.Filter = "\"CSV Files|*.csv|TXT Files|*.txt\"";
-            this.openFileDialog.Multiselect = true;
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.viewSourceToolStripMenuItem});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 22);
-            this.helpToolStripMenuItem.Text = "&Help";
-            // 
-            // viewSourceToolStripMenuItem
-            // 
-            this.viewSourceToolStripMenuItem.Name = "viewSourceToolStripMenuItem";
-            this.viewSourceToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F7)));
-            this.viewSourceToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
-            this.viewSourceToolStripMenuItem.Text = "View &Source";
-            this.viewSourceToolStripMenuItem.Click += new System.EventHandler(this.viewSourceToolStripMenuItem_Click);
             // 
             // pictureBox1
             // 
@@ -315,29 +295,46 @@
             this.pictureBox1.ImageLocation = "";
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(908, 258);
+            this.pictureBox1.Size = new System.Drawing.Size(908, 221);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // imageSourceReference
             // 
+            this.imageSourceReference.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.imageSourceReference.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.referenceToolStripMenuItem});
             this.imageSourceReference.Name = "imageSourceReference";
-            this.imageSourceReference.Size = new System.Drawing.Size(181, 48);
+            this.imageSourceReference.Size = new System.Drawing.Size(228, 52);
             // 
             // referenceToolStripMenuItem
             // 
             this.referenceToolStripMenuItem.Name = "referenceToolStripMenuItem";
-            this.referenceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.referenceToolStripMenuItem.Size = new System.Drawing.Size(227, 48);
             this.referenceToolStripMenuItem.Text = "Reference";
             this.referenceToolStripMenuItem.Click += new System.EventHandler(this.referenceToolStripMenuItem_Click);
             // 
+            // tabControl
+            // 
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(9, 11, 9, 11);
+            this.tabControl.Multiline = true;
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(914, 445);
+            this.tabControl.TabIndex = 0;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "\"CSV Files|*.csv|TXT Files|*.txt\"";
+            this.openFileDialog.Multiselect = true;
+            // 
             // MainForm
             // 
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 39F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1070, 589);
             this.Controls.Add(this.splitContainer1);
@@ -348,7 +345,6 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MinimumSize = new System.Drawing.Size(875, 628);
-
             this.Name = "MainForm";
             this.Text = "Well Trajectory Visualizer";
             this.menuStrip1.ResumeLayout(false);
---
+++ UnitTests/Well.cs
@@ -0,0 +1,155 @@
+using System;
+using Xunit;
+using GeometricObject;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace WellTest
+{
+    public class SourcesProperty
+    {
+        private Well well = new Well("Well1");
+
+        [Fact]
+        public void GetSources_ReturnSources()
+        {
+            var expectedSource =  new List<string>();
+            var actualSource = well.Sources;
+            Assert.Equal(expectedSource, actualSource);
+        }
+
+        [Fact]
+        public void InputSources_SetSources()
+        {
+            var expectedSource = new List<string>();
+            expectedSource.Add("Source1");
+            well.Sources = expectedSource;
+            var actualSource = well.Sources;
+            Assert.Equal(expectedSource, actualSource);
+        }
+    }
+
+    public class TrajectoriesProperty
+    {
+        private Well well = new Well("Well1");
+
+        [Fact]
+        public void GetTrajectories_ReturnTrajectories()
+        {
+            var expectedTrajectory = new List<Trajectory>();
+            var actualTrajectory = well.Trajectories;
+            Assert.Equal(expectedTrajectory, actualTrajectory);
+        }
+
+        [Fact]
+        public void InputTrajectories_SetTrajectories()
+        {
+            var expectedTrajectories = new List<Trajectory>();
+            Trajectory trajectory1 = new Trajectory("source", "wellName", "trajectoryName");
+            expectedTrajectories.Add(trajectory1);
+            well.Trajectories = expectedTrajectories;
+            var actualTrajectory = well.Trajectories;
+            Assert.Equal(expectedTrajectories, actualTrajectory);
+        }
+    }
+
+    public class TrajectoryCountProperty
+    {
+        private Well well = new Well("Well1");
+
+        [Fact]
+        public void GetTrajectoryCount_ReturnTrajectoryCount()
+        {
+            var expectedTrajectoryCount = 0;
+            var actualTrajectoryCount = well.TrajectoryCount;
+            Assert.Equal(expectedTrajectoryCount, actualTrajectoryCount);
+        }
+
+        [Fact]
+        public void InputTrajectoryCount_SetTrajectoryCount()
+        {
+            var expectedTrajectoryCount = 5;
+            well.TrajectoryCount = expectedTrajectoryCount;
+            var actualTrajectoryCount = well.TrajectoryCount;
+            Assert.Equal(expectedTrajectoryCount, actualTrajectoryCount);
+        }
+    }
+
+    public class WellNameProperty
+    {
+        private Well well = new Well("Well1");
+
+        [Fact]
+        public void GetWellName_ReturnWellName()
+        {
+            var expectedWellName = "Well1";
+            var actualWellName = well.WellName;
+            Assert.Equal(expectedWellName, actualWellName);
+        }
+
+        [Fact]
+        public void InputWellName_SetWellName()
+        {
+            var expectedWellName = "ExpectedWellName";
+            well.WellName = expectedWellName;
+            var actualWellName = well.WellName;
+            Assert.Equal(expectedWellName, actualWellName);
+        }
+    }
+
+    public class WellConstructor
+    {
+        [Fact]
+        public void InputWellName_SetWellName()
+        {
+            var expectedWellName = "Well1";
+            Well well = new Well(expectedWellName);
+            var actualWellName = well.WellName;
+            Assert.Equal(expectedWellName, actualWellName);
+        }
+
+        [Fact]
+        public void InitializeSources()
+        {
+            Well well = new Well("Well1");
+            var expected = new List<string>();
+            var actual = well.Sources;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InitializeTrajectories()
+        {
+            Well well = new Well("Well1");
+            var expected = new List<Trajectory>();
+            var actual = well.Trajectories;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InitializeTrajectoryCount()
+        {
+            Well well = new Well("Well1");
+            var expected = 0;
+            var actual = well.TrajectoryCount;
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class AddTrajectory
+    {
+        private Well well = new Well("Well1");
+        [Fact]
+        private void InputTrajectory_AddTrajectory()
+        {
+            var expectedTrajectories = new List<Trajectory>();
+            Trajectory trajectory1 = new Trajectory("source", "wellName", "trajectoryName");
+            trajectory1.PolyLineNodes.Add(new Vector3(1,1,1));
+            expectedTrajectories.Add(trajectory1);
+            well.AddTrajectory(trajectory1);
+            var actualTrajectories = well.Trajectories;
+
+            Assert.Equal(expectedTrajectories, actualTrajectories);
+        }
+    }
+}
