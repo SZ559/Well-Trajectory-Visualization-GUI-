6bc020c7a10507bd2a39d0d2781ad03485c26677
LIU Lin <8675075+kristol07@users.noreply.github.com>
2020-05-24 20:12:46 +0800

    Add unit for axis; update user story: zoom & unit (#59)
    * Update user story: zoom & unit
    
    * Add unit property for trajectory; Add unit for axis

---
+++ GeometricObject/DistanceUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObject
+{
+    [Flags]
+    public enum DistanceUnit
+    {
+        Meter,
+        Feet,
+    }
+}
